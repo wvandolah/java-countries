8e5e52463a4547b45aa8db4917c08026d35fb632
William VanDolah <william.vandolah@gmail.com>
2019-02-19 19:06:53 -0600

    able to filter by length of string passed in

--- src/main/java/com/coutries/demo/NamesController.java
+++ src/main/java/com/coutries/demo/NamesController.java
@@ -11,17 +11,30 @@ import java.util.ArrayList;
 @RequestMapping("/names")
 public class NamesController {
 
-    @RequestMapping("/all")
-    public ArrayList<Country> getAllCountries(){
+    private void sortCountry(){
         DemoApplication.countryList.countryList.sort((c1, c2) ->
                 c1.getName().compareToIgnoreCase(c2.getName()));
+    }
+    @RequestMapping("/all")
+    public ArrayList<Country> getAllCountries(){
+        this.sortCountry();
         return DemoApplication.countryList.countryList;
     }
 // /names/begin?letter=
     @RequestMapping("/begin")
     public ArrayList<Country> getAllBeginWith(
             @RequestParam(value = "letter") String firstLetter){
+        this.sortCountry();
         return DemoApplication.countryList.findCountry(c -> String.valueOf(c.getName().charAt(0)).equals(firstLetter.toUpperCase()));
 
+    }
+
+//    /names/size?letters=
+    @RequestMapping("/size")
+    public ArrayList<Country> getAllWithLength(
+            @RequestParam(value = "letters") String letters){
+        int length = letters.length();
+        this.sortCountry();
+        return DemoApplication.countryList.findCountry(c -> c.getName().length() >= length);
     }
 }
